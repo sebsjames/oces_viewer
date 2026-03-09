d00ff7c01dcb9e58275739057ccf3e193f88969c
Seb James <sebj_coder3@pm.me>
2026-03-09 19:28:37 +0000

    This compiles into a library file

--- reader_lib.cpp
+++ reader_lib.cpp
@@ -4,5 +4,5 @@
 template void oces::get_buffer<float>(const tinygltf::Model&, const int32_t, std::vector<float>&);
 template void oces::get_buffer<uint32_t>(const tinygltf::Model&, const int32_t, std::vector<uint32_t>&);
 template void oces::get_buffer<uint16_t>(const tinygltf::Model&, const int32_t, std::vector<uint16_t>&);
-//template class sm::vec<float, 3>;
-//template void oces::get_buffer<float>(const tinygltf::Model&, const int32_t, std::vector<sm::vec<float, 3>>&);
+template class sm::vec<float, 3>;
+template void oces::get_buffer<sm::vec<float, 3>>(const tinygltf::Model&, const int32_t, std::vector<sm::vec<float, 3>>&);
