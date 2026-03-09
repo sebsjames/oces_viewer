98fc7b5cc0b1572d84af195f582d82739d47a279
Seb James <sebj_coder3@pm.me>
2026-03-09 17:35:15 +0000

    Almost there

--- reader_lib.cpp
+++ reader_lib.cpp
@@ -1,7 +1,8 @@
 #include <cstdint>
 #define OCES_READER_IMPLEMENTATION 1
 #include <oces/reader>
-template class oces::get_buffer<uint32_t>;
-template class oces::get_buffer<uint16_t>;
-template class oces::get_buffer<float>;
-template class oces::get_buffer<sm::vec<float, 3>>;
+template void oces::get_buffer<float>(const tinygltf::Model&, const int32_t, std::vector<float>&);
+template void oces::get_buffer<uint32_t>(const tinygltf::Model&, const int32_t, std::vector<uint32_t>&);
+template void oces::get_buffer<uint16_t>(const tinygltf::Model&, const int32_t, std::vector<uint16_t>&);
+//template class sm::vec<float, 3>;
+//template void oces::get_buffer<float>(const tinygltf::Model&, const int32_t, std::vector<sm::vec<float, 3>>&);
